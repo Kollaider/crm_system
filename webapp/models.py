28256a9739fffae38cceb1c2e79fc9c5b955f8f1
Kollaider <kolja87@tut.by>
2023-02-21 00:08:25 +0300

    added CountryField to Counry model from django_countries library

--- webapp/models.py
+++ webapp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django_countries.fields import CountryField
 
 
 class CreateTimeMixin(models.Model):
@@ -23,6 +24,7 @@ class Company(CreateTimeMixin, UpdateTimeMixin, models.Model):
     name = models.CharField(max_length=50)
     logo = models.ImageField(upload_to='company_logos', )
     scope = models.ForeignKey('Scope', on_delete=models.SET_NULL, null=True, blank=True)
+    country = CountryField(blank_label='выберите страну', blank=True, null=True)
 
     def __str__(self):
         return self.name
