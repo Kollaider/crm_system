077b663716e6494c22328ef15937f3896a47b179
Kollaider <kolja87@tut.by>
2023-03-03 21:59:57 +0300

    1. CBV for Company; 2. refactor templates; 3. Form for Office.

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,12 +4,9 @@ from django.urls import path
 from webapp.views import *
 
 urlpatterns = [
-    path('', index),
-    # path('company/', company, name='company'),
+    path('', index, name='home'),
     path('company/', CompanyView.as_view(), name='company'),
-    # path('company/<int:pk>/', company_detail, name='company_detail'),
     path('company/<int:pk>/', CompanyDetailView.as_view(), name='company_detail'),
-    # path('about/', about, name='about'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('login/', CRMLogin.as_view(), name='login'),
 ]
